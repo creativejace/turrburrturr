36537a0e4fc821eb9163bc106265ad247d42bd96
creativejace <jacetsparks@gmail.com>
2020-11-14 15:43:47 -0800

    Initial Commit

---
+++ turrburrturr/turrburrturrApp.swift
@@ -0,0 +1,17 @@
+//
+//  turrburrturrApp.swift
+//  turrburrturr
+//
+//  Created by Jace Sparks on 11/14/20.
+//
+
+import SwiftUI
+
+@main
+struct turrburrturrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
